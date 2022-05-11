56d4161b6b744d9a4b37902062020733805145c7
Juanma Pintor <juanmapintor22@gmail.com>
2022-05-11 20:57:20 -0300

    Vista responsive de establecimientos con GMaps

---
+++ src/app/components/establecimientos/ver-establecimientos/mas-info/mas-info.component.ts
@@ -0,0 +1,63 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { map, Observable, of, shareReplay } from 'rxjs';
+import { Domicilio } from 'src/app/models/domicilio';
+import { EstablecimientoDetalles } from 'src/app/models/establecimiento';
+
+@Component({
+  selector: 'app-mas-info',
+  templateUrl: './mas-info.component.html',
+  styleUrls: ['./mas-info.component.scss']
+})
+export class MasInfoComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  @Input('establecimientoDetalles') establecimientoDetalles!: EstablecimientoDetalles;
+  center: google.maps.LatLngLiteral = {
+    lat: -40,
+    lng: -80
+  }
+  marker = {
+    position: {
+      lat: -40,
+      lng: -80,
+    },
+    label: {
+      color: 'red',
+      text: 'Establecimiento',
+    },
+    title: 'Nombre...',
+    options: { animation: google.maps.Animation.BOUNCE },
+  }
+  
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) {
+   
+  }
+  ngOnInit(): void { 
+    this.center = {
+      lat: this.establecimientoDetalles?.domicilio.lat || -40,
+      lng: this.establecimientoDetalles?.domicilio.lng || -80
+    };
+
+    this.marker.position = {
+      lat: this.establecimientoDetalles?.domicilio.lat || -40,
+      lng: this.establecimientoDetalles?.domicilio.lng || -80
+    };
+
+    this.marker.title = this.establecimientoDetalles?.nombre || 'Vacio...';
+    this.marker.label.text = this.establecimientoDetalles?.nombre || 'Vacio...';
+  }
+
+
+  getDomicilioString(domicilio: Domicilio) {
+    return `${domicilio.calle} ${domicilio.cardinalidad ? `(${domicilio.cardinalidad})` : ''} ${domicilio.numero || 'S/N'}; ${domicilio.provincia}, ${domicilio.departamento}, ${domicilio.localidad}; ${domicilio.barrio ? `Barrio: ${domicilio.barrio}` : ''}; Cod. Postal: ${domicilio.cod_postal}; ${domicilio.observacion || 'Sin Observaciones.'}`;
+  }
+
+}
--- src/app/components/establecimientos/ver-establecimientos/ver-establecimientos.component.ts
+++ src/app/components/establecimientos/ver-establecimientos/ver-establecimientos.component.ts
@@ -1,16 +1,28 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { throwToolbarMixedModesError } from '@angular/material/toolbar';
-import { map, Observable, shareReplay, tap } from 'rxjs';
+import { catchError, map, Observable, of, shareReplay } from 'rxjs';
+import { Domicilio } from 'src/app/models/domicilio';
+import { EstablecimientoDetalles, EstablecimientoLista } from 'src/app/models/establecimiento';
+import { Page } from 'src/app/models/page';
 import { EstablecimientoService } from 'src/app/services/establecimiento.service';
+import { GLOBAL } from 'src/app/services/global';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ver-establecimientos',
   templateUrl: './ver-establecimientos.component.html',
   styleUrls: ['./ver-establecimientos.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*', minHeight: '200px' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class VerEstablecimientosComponent implements AfterViewInit {
   isHandset$: Observable<boolean> = this.breakpointObserver
@@ -20,9 +32,12 @@ export class VerEstablecimientosComponent implements AfterViewInit {
       shareReplay()
     );
 
-  loading = true;
+ 
 
-  establecimientosDefault: any[] = [
+  loading = true;
+  loadingEstablecimientos: boolean[] = [];
+
+  establecimientosDefault = [
     {
       cue: 'XXXXXXXXX',
       nombre: 'Nombre...',
@@ -49,13 +64,21 @@ export class VerEstablecimientosComponent implements AfterViewInit {
       mail: 'mail@mail...'
     }
   ];
-  establecimientos: any[] = [];
-  establecimientosPage: any;
+  establecimientos: EstablecimientoLista[] = [];
+  establecimientosPage!: Page<EstablecimientoLista>;
+  establecimientoDetalles!: EstablecimientoDetalles;
+
+  indiceExpandido = -1;
+
   dataSourceEstablecimientos = new MatTableDataSource<any>(this.establecimientosDefault);
-  displayedColumnsEstablecimientos: string[] = [
+  displayedColumnsEstablecimientosFull: string[] = [
     'cue',
     'nombre',
     'mail',
+    'acciones'
+  ];
+  displayedColumnsEstablecimientosHandset: string[] = [
+    'nombre',
     'acciones'
   ];
 
@@ -64,7 +87,9 @@ export class VerEstablecimientosComponent implements AfterViewInit {
   constructor(
     private breakpointObserver: BreakpointObserver,
     private _establecimientoService: EstablecimientoService
-  ) { }
+  ) {
+    
+  }
 
   ngAfterViewInit(): void {
     this.loadEstablecimientos(1);
@@ -84,7 +109,26 @@ export class VerEstablecimientosComponent implements AfterViewInit {
     this.paginator.pageSize = this.establecimientosPage.per_page;
     this.paginator.pageIndex = this.establecimientosPage.current_page - 1;
   }
-
+  private configTabla() {
+    this.indiceExpandido = -1;
+    this.loadingEstablecimientos = new Array(this.establecimientos.length).fill(false);
+  }
+  public async expandirRow(index: number, establecimiento: any) {
+    this.loadingEstablecimientos[index] = true;
+    this.indiceExpandido = this.indiceExpandido == index ? -1 : index;
+    try {
+      this.establecimientoDetalles = await this._establecimientoService.show(establecimiento.establecimientoID);
+      this.loadingEstablecimientos[index] = false;
+    } catch (error: any) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error :(',
+        html: 'No se pudo cargar el establecimiento seleccionado <br>' + error.message
+      }).then(() => {
+        this.indiceExpandido = -1;
+      });
+    }
+  }
   private async loadEstablecimientos(page: number) {
     this.establecimientos = [];
     this.loading = true;
@@ -92,6 +136,7 @@ export class VerEstablecimientosComponent implements AfterViewInit {
       this.establecimientosPage = await this._establecimientoService.index(page);
       this.configEstablecimiento();
       this.configPaginator();
+      this.configTabla();
       this.loading = false;
     } catch (error: any) {
       Swal.fire({
