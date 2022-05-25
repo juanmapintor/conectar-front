3d7ae461958564974e4218918f238d623e2fc41e
Juanma Pintor <juanmapintor22@gmail.com>
2022-05-25 00:24:56 -0300

    Configuracion de environments y build para prod

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,7 +1,8 @@
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 export const GLOBAL = {
-  API_URL: 'http://localhost:8000/api/',
+  API_URL: environment.apiURL,
   JSON_HEADERS: {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
